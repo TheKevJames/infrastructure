0d4e0a14ff4e1b337f1cd583d8699017f0272409
Kevin James <KevinJames@thekev.in>
2017-01-30 15:31:57 -0500

    feat(terraform): add mail MX records

--- terraform/mail.tf
+++ terraform/mail.tf
@@ -11,3 +11,19 @@ resource "cloudflare_record" "spf" {
     value = "v=spf1 include:mailgun.org ~all"
     type = "TXT"
 }
+
+resource "cloudflare_record" "mxa" {
+    domain = "${var.cloudflare_domain}"
+    name = "@"
+    value = "mxa.mailgun.org"
+    type = "MX"
+    priority = 10
+}
+
+resource "cloudflare_record" "mxb" {
+    domain = "${var.cloudflare_domain}"
+    name = "@"
+    value = "mxb.mailgun.org"
+    type = "MX"
+    priority = 10
+}
