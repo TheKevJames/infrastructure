18ceee2ea34468a49a21b73e5428d066ef4b3040
Kevin James <KevinJames@thekev.in>
2018-05-05 02:04:14 -0700

    chore(github): repo cleanup

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,89 +1,89 @@
 resource "cloudflare_record" "base" {
-    domain = "${var.cloudflare_domain}"
-    name = "@"
-    type = "A"
+  domain = "${var.cloudflare_domain}"
+  name   = "@"
+  type   = "A"
 
-    proxied = false
+  proxied = false
 
-    value = "${google_compute_address.gcebox.address}"
+  value = "${google_compute_address.gcebox.address}"
 }
 
 resource "cloudflare_record" "home" {
-    domain = "${var.cloudflare_domain}"
-    name = "home"
-    type = "A"
+  domain = "${var.cloudflare_domain}"
+  name   = "home"
+  type   = "A"
 
-    proxied = false
+  proxied = false
 
-    value = "24.23.233.137"
+  value = "24.23.233.137"
 }
 
 resource "cloudflare_record" "jarvis" {
-    domain = "${var.cloudflare_domain}"
-    name = "jarvis"
-    type = "CNAME"
+  domain = "${var.cloudflare_domain}"
+  name   = "jarvis"
+  type   = "CNAME"
 
-    proxied = false
+  proxied = false
 
-    value = "${var.cloudflare_domain}"
+  value = "${var.cloudflare_domain}"
 }
 
 resource "cloudflare_record" "status" {
-    domain = "${var.cloudflare_domain}"
-    name = "status"
-    type = "CNAME"
+  domain = "${var.cloudflare_domain}"
+  name   = "status"
+  type   = "CNAME"
 
-    proxied = false
+  proxied = false
 
-    value = "stats.uptimerobot.com"
+  value = "stats.uptimerobot.com"
 }
 
 resource "cloudflare_record" "www" {
-    domain = "${var.cloudflare_domain}"
-    name = "www"
-    type = "CNAME"
+  domain = "${var.cloudflare_domain}"
+  name   = "www"
+  type   = "CNAME"
 
-    proxied = false
+  proxied = false
 
-    value = "${var.cloudflare_domain}"
+  value = "${var.cloudflare_domain}"
 }
 
 resource "cloudflare_record" "youshouldread" {
-    domain = "${var.cloudflare_domain}"
-    name = "youshouldread"
-    type = "CNAME"
+  domain = "${var.cloudflare_domain}"
+  name   = "youshouldread"
+  type   = "CNAME"
 
-    proxied = false
+  proxied = false
 
-    value = "${var.cloudflare_domain}"
+  value = "${var.cloudflare_domain}"
 }
 
 resource "cloudflare_record" "youshouldread_api" {
-    domain = "${var.cloudflare_domain}"
-    name = "api.youshouldread"
-    type = "CNAME"
+  domain = "${var.cloudflare_domain}"
+  name   = "api.youshouldread"
+  type   = "CNAME"
 
-    proxied = false
+  proxied = false
 
-    value = "${var.cloudflare_domain}"
+  value = "${var.cloudflare_domain}"
 }
 
 resource "cloudflare_record" "ysr" {
-    domain = "${var.cloudflare_domain}"
-    name = "ysr"
-    type = "CNAME"
+  domain = "${var.cloudflare_domain}"
+  name   = "ysr"
+  type   = "CNAME"
 
-    proxied = false
+  proxied = false
 
-    value = "${var.cloudflare_domain}"
+  value = "${var.cloudflare_domain}"
 }
 
 resource "cloudflare_record" "ysr_api" {
-    domain = "${var.cloudflare_domain}"
-    name = "api.ysr"
-    type = "CNAME"
+  domain = "${var.cloudflare_domain}"
+  name   = "api.ysr"
+  type   = "CNAME"
 
-    proxied = false
+  proxied = false
 
-    value = "${var.cloudflare_domain}"
+  value = "${var.cloudflare_domain}"
 }
--- terraform/mail.tf
+++ terraform/mail.tf
@@ -1,43 +1,43 @@
 resource "cloudflare_record" "email" {
-    domain = "${var.cloudflare_domain}"
-    name = "email"
-    type = "CNAME"
+  domain = "${var.cloudflare_domain}"
+  name   = "email"
+  type   = "CNAME"
 
-    proxied = false
+  proxied = false
 
-    value = "mailgun.org"
+  value = "mailgun.org"
 }
 
 resource "cloudflare_record" "dkim" {
-    domain = "${var.cloudflare_domain}"
-    name = "krs._domainkey"
-    type = "TXT"
+  domain = "${var.cloudflare_domain}"
+  name   = "krs._domainkey"
+  type   = "TXT"
 
-    value = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCT7TjG15xvCb7Bl1gAF8VrSHr4paJqkJLiGv+Q9YMVQO9+c8MAwGQcLTxiiMNY52jFcjhPIeg0OZOR+ORfKlxUQHCj1EzqQWoTCcV95RKwCudi2duh4Wqv+sZkqs//FRX0fEIdBS5K79vDftkWgeKqtbiskHa+eVxdMj6aPIDMrwIDAQAB"
+  value = "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCT7TjG15xvCb7Bl1gAF8VrSHr4paJqkJLiGv+Q9YMVQO9+c8MAwGQcLTxiiMNY52jFcjhPIeg0OZOR+ORfKlxUQHCj1EzqQWoTCcV95RKwCudi2duh4Wqv+sZkqs//FRX0fEIdBS5K79vDftkWgeKqtbiskHa+eVxdMj6aPIDMrwIDAQAB"
 }
 
 resource "cloudflare_record" "spf" {
-    domain = "${var.cloudflare_domain}"
-    name = "@"
-    type = "TXT"
+  domain = "${var.cloudflare_domain}"
+  name   = "@"
+  type   = "TXT"
 
-    value = "v=spf1 include:mailgun.org ~all"
+  value = "v=spf1 include:mailgun.org ~all"
 }
 
 resource "cloudflare_record" "mxa" {
-    domain = "${var.cloudflare_domain}"
-    name = "@"
-    type = "MX"
+  domain = "${var.cloudflare_domain}"
+  name   = "@"
+  type   = "MX"
 
-    priority = 10
-    value = "mxa.mailgun.org"
+  priority = 10
+  value    = "mxa.mailgun.org"
 }
 
 resource "cloudflare_record" "mxb" {
-    domain = "${var.cloudflare_domain}"
-    name = "@"
-    type = "MX"
+  domain = "${var.cloudflare_domain}"
+  name   = "@"
+  type   = "MX"
 
-    priority = 10
-    value = "mxb.mailgun.org"
+  priority = 10
+  value    = "mxb.mailgun.org"
 }
