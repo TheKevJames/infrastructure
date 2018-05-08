cb9f6fbdd1df8010192c347cbbdea9d1634a3d50
Kevin James <KevinJames@thekev.in>
2018-05-07 22:47:55 -0700

    chore(dns): clean up forwarding rules

--- site.tf
+++ site.tf
@@ -8,12 +8,41 @@ resource "cloudflare_record" "base" {
   value = "c.storage.googleapis.com"
 }
 
-resource "cloudflare_record" "www" {
-  domain = "${var.domain}"
-  name   = "www"
-  type   = "CNAME"
+resource "cloudflare_page_rule" "redirect-cv" {
+  zone     = "${var.domain}"
+  target   = "${var.domain}/cv"
+  priority = 1
 
-  proxied = true
+  actions = {
+    forwarding_url {
+      url         = "${var.artifacts_url}/cv.pdf"
+      status_code = 301
+    }
+  }
+}
 
-  value = "${var.domain}"
+resource "cloudflare_page_rule" "redirect-hexclock" {
+  zone     = "${var.domain}"
+  target   = "${var.domain}/hexclock"
+  priority = 2
+
+  actions = {
+    forwarding_url {
+      url         = "${var.artifacts_url}/hexclock/hexclock.html"
+      status_code = 301
+    }
+  }
+}
+
+resource "cloudflare_page_rule" "redirect-quotes" {
+  zone     = "${var.domain}"
+  target   = "${var.domain}/quotes"
+  priority = 3
+
+  actions = {
+    forwarding_url {
+      url         = "${var.artifacts_url}/quotes/quotes.html"
+      status_code = 301
+    }
+  }
 }
