717b924c231e72f7e994a2febd49acd6a433cc31
Kevin James <KevinJames@thekev.in>
2020-02-17 17:32:50 -0800

    chore(deps): pin exact provider versions

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 provider "cloudflare" {
-  version = "~> 1.0"
+  version = "1.18.1"
 
   email = "${var.cloudflare_email}"
   token = "${var.cloudflare_token}"
 }
 
 provider "google" {
-  version = "~> 1.0"
+  version = "1.20.0"
 
   project = "thekevjames-175823"
   region  = "us-central1"
