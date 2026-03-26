50d0991a54d16e01f9e4d64f3561f91cb457564b
Kevin James <KevinJames@thekev.in>
2026-03-26 14:26:00 +0000

    refactor(lint): enable tflint

--- variable.tf
+++ variable.tf
@@ -1,17 +1,23 @@
 variable "artifacts_url" {
   default = "https://storage.googleapis.com/thekevjames-artifacts"
+  type    = string
 }
 
-variable "cloudflare_email" {}
+variable "cloudflare_email" {
+  type = string
+}
 
 variable "cloudflare_token" {
   sensitive = true
+  type      = string
 }
 
 variable "domain" {
   default = "thekev.in"
+  type    = string
 }
 
 variable "zone" {
   default = "b16202b6cdadb4d72e460f6c2be19fa7"
+  type    = string
 }
